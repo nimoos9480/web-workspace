f652344ed011dcefbf6a12aa627fe282095af76a
Shin soomin <soomin6372@gmail.com>
2023-08-22 16:47:08 +0900

    feat_수정사항

--- 07_CafeMember/src/servlet/model/MemberDAOTemplate.java
+++ 07_CafeMember/src/servlet/model/MemberDAOTemplate.java
@@ -12,9 +12,9 @@ public interface MemberDAOTemplate {
 	void closeAll(PreparedStatement ps, Connection conn) throws SQLException;
 	void closeAll(ResultSet rs, PreparedStatement ps, Connection conn) throws SQLException;
 	
-	void insertMember(MemberVO vo) throws SQLException;
-	ArrayList<MemberVO> showAllMember() throws SQLException;
-	MemberVO findByNameMember(String name) throws SQLException;
+	void insertMember(MemberVO vo) throws SQLException; // 리턴값이 없으니까 void
+	ArrayList<MemberVO> showAllMember() throws SQLException; // 리턴값이 ArrayList, 매개변수는 없음
+	MemberVO findByNameMember(String name) throws SQLException; // 매개변수 name을 받아서 MemberVO로 리턴
 	
 }
 
--- 13_MemberJSTL/src/servlet/model/vo/MemberDTO.java
+++ 13_MemberJSTL/src/servlet/model/vo/MemberDTO.java
@@ -4,6 +4,7 @@ package servlet.model.vo;
  * VO(Value Object), DTO(Data Transfer Object) : 데이터 전송 및 저장 객체
  * - getter/setter만 가지는 것을 DTO
  * - 실상 구별없이 사용함
+ * - 다만, 생성자가 없기 때문에 객체 생성을 일일히 해줘야 함
  * */
 
 public class MemberDTO {
